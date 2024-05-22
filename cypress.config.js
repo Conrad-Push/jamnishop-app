36280a74c1f7025edb5ad25019bf1b8d8a20866e
Conrad-Push <konrad.pusz7@gmail.com>
2024-05-22 14:24:42 +0200

    Add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,17 @@ module.exports = defineConfig({
   e2e: {
     // baseUrl: "https://jamnishop.web.app/",
     baseUrl: "http://localhost:5173/",
+    reporter: "cypress-mochawesome-reporter",
+    screenshotsFolder: "cypres/reports/screenshots",
+    reporterOptions: {
+      mochaFile: "results/cypress-report-[hash].xml",
+      toConsole: true,
+    },
+    video: false,
+    retries: {
+      runMode: 1,
+      openMode: 0,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
